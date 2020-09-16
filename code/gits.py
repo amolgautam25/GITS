c9a98d3b3c043a3a4436abc147389ec9f9ac82b3
Tanay Agrawal <tagrawa3@ncsu.edu>
2020-09-16 18:03:02 -0400

    adding the entry point for the gits project and hello world command

---
+++ code/gits.py
@@ -0,0 +1,13 @@
+import os
+import sys
+import argparse
+from gits_hello import gits_hello_world
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+
+gits_hello_subparser = subparsers.add_parser('hello_world')
+gits_hello_subparser.set_defaults(func=gits_hello_world)
+
+args = parser.parse_args()
+args.func(args)
---
+++ code/gits_hello.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+
+def gits_hello_world(args):
+    """
+    Function that prints hello message
+    to user console
+    """
+    print("Hello from GITS Commandline Tools")
