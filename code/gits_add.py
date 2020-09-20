18c5b2c1bf2b8e16ee9059bd1f5d929acc7aa92a
Tanay Agrawal <tagrawa3@ncsu.edu>
2020-09-20 16:50:09 -0400

    ISSUE 19: adding support for git add workflow

---
+++ code/gits_add.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import os
+import sys
+from subprocess import Popen, PIPE
+
+
+def gits_add_func(args):
+    """
+    Function that adds files as passed
+    to the gits add command.
+    Performs operation as similar to git
+    add command
+    """
+    try:
+        subprocess_command = list()
+        subprocess_command.append("git")
+        subprocess_command.append("add")
+        file_names_list = args.file_names
+        total_files = len(file_names_list)
+        if total_files == 0:
+            #do nothing
+            pass
+        else:
+            for i in range(0, total_files):
+                subprocess_command.append(file_names_list[i])
+            process = Popen(subprocess_command, stdout=PIPE, stderr=PIPE)
+            stdout, stderr = process.communicate()
+
+    except Exception as e:
+        print("ERROR: gits add command caught an exception")
+        print("ERROR: {}".format(str(e)))
+        return False
+
+    return True
