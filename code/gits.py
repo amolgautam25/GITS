207e7c6f4b1c957b0d74ca6b7af9a08bc177c766
Srujana Rachakonda <srujana13rachakonda@gmail.com>
2020-09-20 22:43:40 -0400

    Added gits upstream code

--- code/gits.py
+++ code/gits.py
@@ -6,6 +6,7 @@ import argparse
 from gits_hello import gits_hello_world
 from gits_add import gits_add_func
 from gits_commit import gits_commit_func
+from gits_setupstream import upstream
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
@@ -21,6 +22,12 @@ gits_commit_subparser = subparsers.add_parser('commit')
 gits_commit_subparser.add_argument('-m', required=True, help='git commit message')
 gits_commit_subparser.add_argument('--amend', action='store_true', help='amend commit with previous commit message')
 gits_commit_subparser.set_defaults(func=gits_commit_func)
+
+gits_upstream_subparser = subparsers.add_parser('upstream')
+gits_upstream_subparser.add_argument('--remote', help='the remote branch we want to use')
+gits_upstream_subparser.add_argument('--local', help="local branch we want to track")
+gits_upstream_subparser.add_argument('--upstream', help="the upstream branch we want to track to")
+gits_upstream_subparser.set_defaults(func=upstream)
 
 args = parser.parse_args()
 args.func(args)
