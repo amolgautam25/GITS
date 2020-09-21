ee337d1626797a7949dfad8721a8ad3fd707f58b
Srujana Rachakonda <srujana13rachakonda@gmail.com>
2020-09-21 00:06:52 -0400

    create branch code issue 3

--- code/gits.py
+++ code/gits.py
@@ -9,6 +9,7 @@ from gits_add import gits_add_func
 from gits_commit import gits_commit_func
 from gits_set import gits_set_func
 from gits_setupstream import upstream
+from gits_create_branch import create_branch
 
 logger_status = init_gits_logger()
 if not logger_status:
@@ -17,9 +18,6 @@ if not logger_status:
 
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers()
-
-gits_hello_subparser = subparsers.add_parser('hello_world')
-gits_hello_subparser.set_defaults(func=gits_hello_world)
 
 gits_set_subparser = subparsers.add_parser('set')
 gits_set_subparser.add_argument('--parent', help='git parent branch')
@@ -33,6 +31,11 @@ gits_commit_subparser = subparsers.add_parser('commit')
 gits_commit_subparser.add_argument('-m', required=True, help='git commit message')
 gits_commit_subparser.add_argument('--amend', action='store_true', help='amend commit with previous commit message')
 gits_commit_subparser.set_defaults(func=gits_commit_func)
+
+gits_create_subparser = subparsers.add_parser('create')
+gits_create_subparser.add_argument('-b', help="branch name to create")
+gits_create_subparser.set_defaults(func=create_branch)
+
 
 gits_upstream_subparser = subparsers.add_parser('upstream')
 gits_upstream_subparser.add_argument('--remote', help='the remote branch we want to use')
