ae2ec29b11afc0ede308059d36297e4d52b4c5e2
Tanay Agrawal <tagrawa3@ncsu.edu>
2020-09-21 22:56:49 -0400

    Adding unit test cases for Gits commit code

--- code/test_gits_commit.py
+++ code/test_gits_commit.py
@@ -1,14 +1,14 @@
 import argparse
-import gits_add
+import gits_commit
 from mock import patch
 
 
 @patch("argparse.ArgumentParser.parse_args",
-       return_value=argparse.Namespace(file_names="test_file_names"))
+       return_value=argparse.Namespace(m="test_commit", amend=True))
 @patch("subprocess.Popen", return_value="anything")
-def test_gits_add_func_1(mock_var1, mock_args):
+def test_gits_commit_func_1(mock_var1, mock_args):
     """
     Function to test gits_add, success case
     """
-    test_result = gits_add.gits_add_func(mock_args)
+    test_result = gits_commit.gits_commit_func(mock_args)
     assert True == test_result, "Normal case"
