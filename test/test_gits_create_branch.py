da1714d3fd8792e243098fb72b49833bab09ac37
kenil-shah <kshah9@ncsu.edu>
2020-10-19 18:13:47 -0400

    Updated testing paths

--- test/test_gits_create_branch.py
+++ test/test_gits_create_branch.py
@@ -1,5 +1,11 @@
 import argparse
-import gits_create_branch
+import os
+import sys
+
+os.chdir("../code")
+sys.path.insert(1, os.getcwd())
+
+from gits_create_branch import create_branch
 from mock import patch, Mock
 
 
@@ -9,5 +15,5 @@ def test_git_create_branch_happy_path(mock_popen, mock_args):
     """
     Function to test gits_create_branch, success case
     """
-    test_result = gits_create_branch.create_branch(mock_args)
+    test_result = create_branch(mock_args)
     assert test_result == True
