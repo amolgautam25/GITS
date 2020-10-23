5599e7ce5253abcaa61b267dab8368018dd3890e
Faraaz Kakiwala <faraaz7498@gmail.com>
2020-10-23 04:43:09 -0400

    Update test_gits_track.py
    Added unit test for gits_track

--- test/test_gits_track.py
+++ test/test_gits_track.py
@@ -3,7 +3,6 @@ import os
 import sys
 
 sys.path.insert(1, os.getcwd())
-print(sys.path)
 
 from gits_track import gits_track
 from mock import patch, Mock
@@ -11,7 +10,7 @@ from mock import patch, Mock
 @patch("argparse.ArgumentParser.parse_args",
        return_value=argparse.Namespace(file_names="test_file_names"))
 @patch("subprocess.Popen", return_value="anything")
-def test_gits_track(mock_args):
+def test_gits_track(mock_var, mock_args):
     """
     Function to test gits track, success case
     """
