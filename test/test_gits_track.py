7f669404d7fb6928a547b27b36abf3469b64ca4a
Faraaz Kakiwala <faraaz7498@gmail.com>
2020-10-23 21:40:49 -0400

    Update test_gits_track.py
    test_gits_track now tests the actual code path

--- test/test_gits_track.py
+++ test/test_gits_track.py
@@ -7,12 +7,19 @@ sys.path.insert(1, os.getcwd())
 from gits_track import gits_track
 from mock import patch, Mock
 
+
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    return parser.parse_args(args)
+
+
 @patch("argparse.ArgumentParser.parse_args",
-       return_value=argparse.Namespace(file_names="test_file_names"))
+       return_value=argparse.Namespace(file_names=["test1", "test2"]))
 @patch("subprocess.Popen", return_value="anything")
 def test_gits_track(mock_var, mock_args):
     """
     Function to test gits track, success case
     """
+    mock_args = parse_args(mock_args)
     test_result = gits_track(mock_args)
     assert True == test_result, "Normal case"
