1775a15a940fd0acfe624ce06e1cfd8ecda9baf5
Faraaz Kakiwala <faraaz7498@gmail.com>
2020-10-23 21:43:12 -0400

    Update test_gits_untrack.py
    test_gits_untrack now tests the actual code path

--- test/test_gits_untrack.py
+++ test/test_gits_untrack.py
@@ -7,6 +7,12 @@ sys.path.insert(1, os.getcwd())
 from gits_untrack import gits_untrack
 from mock import patch, Mock
 
+
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    return parser.parse_args(args)
+
+
 @patch("argparse.ArgumentParser.parse_args",
        return_value=argparse.Namespace(file_names="test_file_names"))
 @patch("subprocess.Popen", return_value="anything")
@@ -14,5 +20,6 @@ def test_gits_track(mock_var, mock_args):
     """
     Function to test gits track, success case
     """
+    mock_args = parse_args(mock_args)
     test_result = gits_untrack(mock_args)
     assert True == test_result, "Normal case"
