d8e978308a236f3c1b9494c694843d4db76f8913
Faraaz Kakiwala <faraaz7498@gmail.com>
2020-10-23 21:44:45 -0400

    Update test_gits_undo.py
    test_gits_undo now tests the actual code path

--- test/test_gits_undo.py
+++ test/test_gits_undo.py
@@ -8,6 +8,11 @@ from gits_undo import gits_undo
 from mock import patch
 
 
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    return parser.parse_args(args)
+
+
 @patch("argparse.ArgumentParser.parse_args",
        return_value=argparse.Namespace(file_names="test_file_names"))
 @patch("subprocess.Popen", return_value="anything")
@@ -15,5 +20,6 @@ def test_gits_add_func_1(mock_var1, mock_args):
     """
     Function to test gits_add, success case
     """
+    mock_args = parse_args(mock_args)
     test_result = gits_undo(mock_args)
     assert True == test_result, "Normal case"
