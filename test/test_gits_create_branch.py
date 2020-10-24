112634fb0edec5ba43fe5982cf82f41619e69a1a
Faraaz Kakiwala <faraaz7498@gmail.com>
2020-10-23 21:54:28 -0400

    Update test_gits_create_branch.py
    test_gits_create_branch now tests the actual code path

--- test/test_gits_create_branch.py
+++ test/test_gits_create_branch.py
@@ -8,11 +8,17 @@ from gits_create_branch import create_branch
 from mock import patch, Mock
 
 
+def parse_args(args):
+    parser = argparse.ArgumentParser()
+    return parser.parse_args(args)
+
+
 @patch("argparse.ArgumentParser.parse_args", return_value=argparse.Namespace(b="branch name"))
 @patch("subprocess.Popen")
 def test_git_create_branch_happy_path(mock_popen, mock_args):
     """
     Function to test gits_create_branch, success case
     """
+    mock_args = parse_args(mock_args)
     test_result = create_branch(mock_args)
     assert test_result == True
