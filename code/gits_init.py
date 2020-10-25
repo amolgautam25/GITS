4f516bcfa195aa036d35dd14689ea7b27f18844f
Harshit Patel <harshit.p.patel96@gmail.com>
2020-10-25 18:35:24 -0400

    removed indentation errors

--- code/gits_init.py
+++ code/gits_init.py
@@ -12,21 +12,19 @@ def gits_init(args):
    3) Initialize using a preexisting template
         gits init --template path-to-template
    """ 
-    try:
-        process_commands = ["git", "init"]
-
-        if args.bare is not False and args.bare is not None:
-            process_commands.append("--bare")
-        elif args.template is not None:
-            process_commands.append("--template")
-            process_commands.append(args.template)
-
-        process = Popen(process_commands, stdout=PIPE, stderr=PIPE)
-        stdout, stderr = process.communicate()
-
-    except Exception as e:
-        print("ERROR: gits init command caught an exception")
-        print("ERROR: {}".format(str(e)))
-        return False
-
-    return True
+   try:
+      process_commands = ["git", "init"]
+      if args.bare is not False and args.bare is not None:
+         process_commands.append("--bare")
+      elif args.template is not None:
+         process_commands.append("--template")
+         process_commands.append(args.template)
+      
+      process = Popen(process_commands, stdout=PIPE, stderr=PIPE)
+      stdout, stderr = process.communicate()
+   except Exception as e:
+      print("ERROR: gits init command caught an exception")
+      print("ERROR: {}".format(str(e)))
+      return False
+   
+   return True
