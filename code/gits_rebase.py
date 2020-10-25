ed546399012c981b3608227a5228e024996ffb6e
urvishvasani <ur207vasani@gmail.com>
2020-10-25 10:16:19 -0400

    updated the readme for gits rebase

--- code/gits_rebase.py
+++ code/gits_rebase.py
@@ -8,21 +8,19 @@ def gits_rebase(args):
     This is the most common scenario. The original GIT rebase command is a little un-intuitive and there is always a
     confusion , about the source branch and the destination branch.
 """
-    print(args)
-    print("Hello from GITS command line tools- Rebase")
     try:
         print("Is the rebase on current branch?")
         inp = input("[yes/no][y/n]")
         if inp.lower() == "yes" or inp.lower() == "y":
             process1 = subprocess.Popen(['git', 'rebase', 'master'], stdout=PIPE, stderr=PIPE)
             stdout, stderr = process1.communicate()
-            # print(stdout)
+            print(stdout)
         else:
-            inp2 = input("Enter the name of the branch you want to rebase")
+            inp2 = input("Enter the name of the branch you want to rebase: ")
             # print(inp2)
             process2 = subprocess.Popen(['git', 'checkout', inp2, 'git', 'rebase', 'master'], stdout=PIPE, stderr=PIPE)
             stdout, stderr = process2.communicate()
-            # print(stdout)
+            print(stdout)
     except Exception as e:
         print("ERROR: gits reset command caught an exception")
         print("ERROR: {}".format(str(e)))
